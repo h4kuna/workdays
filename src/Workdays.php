9edd1dcb1d178747c2450c6fb9eaa171dac756f5
Milan Matějček <milan.matejcek@gmail.com>
2026-09-25 09:10:25 +0200

    remove dependency on memoize

--- src/Workdays.php
+++ src/Workdays.php
@@ -7,25 +7,19 @@ use DateTime;
 use DateTimeImmutable;
 use DateTimeInterface;
 use h4kuna\DataType\Date\Convert;
-use h4kuna\Memoize\MemoryStorage;
-use h4kuna\Workdays\Exceptions\InvalidStateException;
 use h4kuna\Workdays\HolidaysProvider\BaseProvider;
 use h4kuna\Workdays\HolidaysProvider\Holiday;
 
 class Workdays
 {
-	use MemoryStorage;
-
 	public function __construct(private /* readonly */ BaseProvider $holidayProvider)
 	{
 	}
-
 
 	public function isWorkday(DateTimeInterface $date): bool
 	{
 		return self::isWeekend($date) === false && $this->isHoliday($date) === false;
 	}
-
 
 	public function isHoliday(DateTimeInterface $date): bool
 	{
@@ -33,19 +27,16 @@ class Workdays
 		return $holiday !== null && $holiday->vacation === false;
 	}
 
-
 	public function isVacation(DateTimeInterface $date): bool
 	{
 		$holiday = $this->holidayProvider->get($date);
 		return $holiday !== null && $holiday->vacation === true;
 	}
 
-
 	public static function isWeekend(DateTimeInterface $date): bool
 	{
 		return $date->format('N') >= 6;
 	}
-
 
 	/**
 	 * @return ($date is DateTime ? DateTime : DateTimeImmutable)
@@ -54,7 +45,6 @@ class Workdays
 	{
 		return $this->moveWorkdays($date, 1);
 	}
-
 
 	public function nextHoliday(DateTime|DateTimeImmutable $date): Holiday
 	{
@@ -66,15 +56,13 @@ class Workdays
 		return $holiday;
 	}
 
-
 	/**
 	 * @return ($date is DateTime ? DateTime : DateTimeImmutable)
 	 */
 	public function moveWorkdays(
 		DateTime|DateTimeImmutable $date,
 		int $numberOfWorkdays,
-	): DateTime|DateTimeImmutable
-	{
+	): DateTime|DateTimeImmutable {
 		$interval = new DateInterval('P1D');
 		$copyDate = Convert::toMutable($date);
 
